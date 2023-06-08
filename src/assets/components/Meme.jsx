c6d6d121ffa5a4f9847651150bc0a55a4e7ffcea
Matthew Klemmer <theklemmercode@outlook.com>
2023-06-07 20:45:35 -0400

    Added functionality to meme function button, sylized meme image, added temporary data.

--- src/assets/components/Meme.jsx
+++ src/assets/components/Meme.jsx
@@ -1,29 +1,43 @@
 import '../css/meme.css'
+import memesData from '../data/memesData'
+import { useState } from 'react'
 
 function Meme() {
-  return (
-    <div className="meme--content">
-        <section className="meme--input">
-            <form className="meme--form">
-                <input 
-                    type="text"
-                    placeholder="Top Text"
-                    className="meme--form-input"
-                />
-                <input 
-                    type="text"
-                    placeholder="Bottom Text"
-                    className="meme--form-input"
-                />
-                <button
-                    className="meme--form-button"
-                >
-                    Get a new meme image 🖼
-                </button>
-            </form> 
-        </section>
-    </div>
-  )
+    const [memeImage, setMemeImage] = useState("")
+
+    function handleClick() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+
+    return (
+        <div className="meme--content">
+            <section className="meme--input">
+                <div className="meme--form">
+                    <input 
+                        type="text"
+                        placeholder="Top Text"
+                        className="meme--form-input"
+                    />
+                    <input 
+                        type="text"
+                        placeholder="Bottom Text"
+                        className="meme--form-input"
+                    />
+                    <button
+                        className="meme--form-button"
+                        onClick={handleClick}
+                    >
+                        Get a new meme image 🖼
+                    </button>
+                </div>
+                <div className="meme--image">
+                    <img src={memeImage}/>
+                </div>
+            </section>
+        </div>
+    )
 }
 
 export default Meme
